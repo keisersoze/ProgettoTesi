27a1283322301a293af3dbc89f6984534d436fe7
Alessio Del Conte <ale.delconte96@gmail.com>
2018-04-05 14:00:10 +0200

    avanzamento

--- src/main/java/app/H2OSim.java
+++ src/main/java/app/H2OSim.java
@@ -17,8 +17,8 @@ public class H2OSim {
     public static final int MU = 3;
     public static final int LAMDA = 3;
     public static final int NTHREADS = 10;
-    public static final int NEVENTS = 100;
-    public static final boolean CANVAS_MODE = false;
+    public static final int NEVENTS = 10000;
+    public static final boolean CANVAS_MODE = true;
 
     //risorse condivise
     public static final MersenneTwister MERSENNE_TWISTER = new MersenneTwister();
