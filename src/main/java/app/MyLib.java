c51571b6ef72cc4a2aae837f28884c5a490d7d9c
Alessio Del Conte <ale.delconte96@gmail.com>
2018-04-15 10:36:31 +0200

    Calcolo neightbors

--- src/main/java/app/MyLib.java
+++ src/main/java/app/MyLib.java
@@ -1,6 +1,11 @@
 package app;
 
+import app.model.Sensor;
+import app.sim.SimContext;
 import org.apache.commons.math3.random.MersenneTwister;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyLib {
     public static double map (double value, double low1, double high1, double low2, double high2) {
@@ -9,5 +14,15 @@ public class MyLib {
 
     public static float random (float min, float max) {
         return min + (int) (new MersenneTwister().nextDouble() * ((max - min) + 1));
+    }
+
+    public static List<Sensor> calculateNeighbors (Sensor sensor, SimContext context) {
+        List<Sensor> myNeighbors = new ArrayList<>();
+        for (Sensor sensor1 : context.getSensors()) {
+            if (sensor.getEuclideanDistance(sensor1) < H2OSim.MAX_DISTANCE / H2OSim.SCALE){    //TODO: deve calcolarla
+                myNeighbors.add(sensor1);
+            }
+        }
+        return myNeighbors;
     }
 }
